84af5de573c8833f6f3d41736a578dbd5fa9be75
Amin <M.amin.ansari@gmail.com>
2015-03-02 17:14:38 +0330

    init
    init

---
+++ pastaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pastaa
+//
+//  Created by Amin on 3/2/15.
+//  Copyright (c) 2015 Reza Sazegarnezhad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pastaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pastaa
+//
+//  Created by Amin on 3/2/15.
+//  Copyright (c) 2015 Reza Sazegarnezhad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
